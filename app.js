1406884b2223476317d35db1fafb6413e9e4ee90
LUKE REYNOLDS <luke.reynolds.92@gmail.com>
2023-10-03 10:20:04 +0100

    update js

--- app.js
+++ app.js
@@ -1,103 +1,113 @@
-// DOM Nodes
+const img1 = document.getElementById("img1");
+const img2 = document.getElementById("img2");
+const img3 = document.getElementById("img3");
 
-let productContainer = document.querySelector("section");
-let image1 = document.querySelector("section img:first-child");
-let image2 = document.querySelector("img:nth-child(2)");
-let image3 = document.querySelector("img:nth-child(3)");
+// make sure the user only has 25 clicks
+let userClicks = 0;
+let maxClicks = 25;
 
-// keep each product in an object
-function odProduct(name, imgPath) {
+// a constructor that makes product objects
+function Product(name) {
   this.name = name;
-  this.imgPath = imgPath;
-  this.timesShown = 0;
-  this.view = 0;
+  this.src = `./images/${name}.jpg`;
+  this.views = 0;
+  this.clicks = 0;
 }
 
-// function to choose a random product
-function getRandomIndex() {
-  return Math.floor(Math.random() * allProducts.length);
+const products = [
+  new Product("bag"),
+  new Product("banana"),
+  new Product("bathroom"),
+  new Product("boots"),
+  new Product("breakfast"),
+  new Product("bubblegum"),
+  new Product("chair"),
+  new Product("cthulhu"),
+  new Product("dog-duck"),
+  new Product("dragon"),
+  new Product("pen"),
+];
+
+// function that randomly gets a index for an item in item
+function randomProdIdx() {
+  return Math.floor(Math.random() * products.length);
 }
 
-// function to render 3 random products
+// make a function that puts 3 random products on the page (using the 3 img tags)
 function renderProducts() {
-  // get 3 random indexes from our product array
-  let product1Index = getRandomIndex();
-  let product2Index = getRandomIndex();
-  let product3Index = getRandomIndex();
+  // get 3 indexs from our products array
+  let prod1 = randomProdIdx();
+  let prod2 = randomProdIdx();
+  let prod3 = randomProdIdx();
 
-  // prevent the two images being the same product
-  while (
-    product1Index === product2Index ||
-    product1Index === product3Index ||
-    product2Index === product3Index
-  ) {
-    product2Index = getRandomIndex();
-    product3Index = getRandomIndex();
+  // make sure they aren't the same
+  while (prod1 === prod2 || prod1 === prod3 || prod2 === prod3) {
+    prod2 = randomProdIdx();
+    prod3 = randomProdIdx();
   }
 
-   // change the imgPath of our 3 images
-   image1.imgPath = allProducts[product1Index].imgPath;
-   image2.imgPath = allProducts[product2Index].imgPath;
-   image3.imgPath = allProducts[product3Index].imgPath;
-   image1.alt = allProducts[product1Index].name;
-   image2.alt = allProducts[product2Index].name;
-   image3.alt = allProducts[product3Index].name;
-  
+  // change the src and alt attributes of our img tags
+  img1.src = products[prod1].src;
+  img2.src = products[prod2].src;
+  img3.src = products[prod3].src;
+  img1.alt = products[prod1].name;
+  img2.alt = products[prod2].name;
+  img3.alt = products[prod3].name;
 
-    // number of times products have been shown
-  allProducts[product1Index].timesShown++;
-  allProducts[product2Index].timesShown++;
-  allProducts[product3Index].timesShown++;
+  // increase the views of the displayed objects
+  products[prod1].views++;
+  products[prod2].views++;
+  products[prod3].views++;
 }
 
-// handle the goat being clicked
-function handleProductClick(event) {
-  // get the name of the product we just clicked
+// handle what happens when click the image
+// the "clicks" property of the image I click to go up by one
+// render 3 new images
+function handleImgClick(event) {
+  // check if the user has run out of clicks
+  if (userClicks === maxClicks) {
+    alert("You have run out of votes");
+    return; // end the function here and don't run the rest
+  }
+
+  // increase the number of times the user has clicked
+  userClicks++;
+
+  // get the name of the clicked product
   let clickedProduct = event.target.alt;
 
-  // check if the click is on an image
-  if (event.target === productContainer) {
-    alert("Please click on an image");
-  } else {
-    // render more goats
-    renderProducts();
+  // increase the clicks of the product
+  for (let i = 0; i < products.length; i++) {
+    if (clickedProduct === products[i].name) {
+      products[i].clicks++;
+      break; // break because we found what we are looking for
+    }
   }
 
-  // increase the clicks of the products
-  // loop through allProducts
-  // for (let i = 0; i < allGoats.length; i++) {
-    // check if the name of the goat in the array, matches the alt tag of our image
-    // if (clickedGoat === allGoats[i].name) {
-      // increase the number of clicks
-     // allGoats[i].clicks++;
-      // stop the for loop because we found the goat
-      // break;
-    }
+  // render 3 more images
+  renderProducts();
+}
+
+img1.addEventListener("click", handleImgClick);
+img2.addEventListener("click", handleImgClick);
+img3.addEventListener("click", handleImgClick);
+
+// a button to view the results
+function showResults() {
+  // put a bunch of lis into a ul
+  const results = document.getElementById("results");
+
+  // loop through our products and make an li for each one
+  for (let i = 0; i < products.length; i++) {
+    const li = document.createElement("li");
+    const product = products[i];
+    li.textContent = `${product.name} was viewed ${product.views} times, and clicked ${product.clicks} times`;
+    results.appendChild(li);
   }
 }
 
+// make the button show the results
+const viewResults = document.getElementById("view-results");
+viewResults.addEventListener("click", showResults);
 
-
-  
-  // make the products
-  const allProducts = [
-    new odProduct("bag", "./201-11/bag.jpg"),
-    new odProduct("banana", "./201-11/banana.jpg"),
-    new odProduct("bathroom", "./201-11/bathroom.jpg"),
-    new odProduct("boots", "./201-11/boots.jpg"),
-    new odProduct("breakfast", "./201-11/breakfast.jpg"),
-    new odProduct("bubblegum", "./201-11/bubblegum.jpg"),
-    new odProduct("chair", "./201-11/chair.jpg"),
-    new odProduct("cthulhu", "./201-11/cthulhu.jpg"),
-    new odProduct("dog-duck", "./201-11/dog-duck.jpg"),
-    new odProduct("dragon", "./201-11/dragon.jpg"),
-    new odProduct("pen", "./201-11/pen.jpg"),
-    new odProduct("pet-sweep", "./201-11/pet-sweep.jpg"),
-    new odProduct("scissors", "./201-11/scissors.jpg"),
-    new odProduct("shark", "./201-11/shark.jpg"),
-    new odProduct("sweep", "./201-11/sweep.jpg"),
-    new odProduct("tauntaun", "./201-11/tauntaun.jpg"),
-    new odProduct("unicorn", "./201-11/unicorn.jpg"),
-    new odProduct("water-can", "./201-11/water-can.jpg"),
-    new odProduct("wine-glass", "./201-11/wine-glass.jpg"),
-  ];
+renderProducts();
