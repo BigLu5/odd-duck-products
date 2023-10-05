6bdc77dac2bd32ffaf55bbea677e0538fdaf9792
LUKE REYNOLDS <luke.reynolds.92@gmail.com>
2023-10-05 14:19:40 +0100

    separate chart

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const img3 = document.getElementById("img3");
 
 // make sure the user only has 25 clicks
 let userClicks = 0;
-const maxClicks = 20;
+const maxClicks = 5;
 
 const products = [];
 
@@ -14,7 +14,8 @@ function Product(name, views, clicks) {
   this.src = `./images/${name}.jpg`;
   this.views = views;
   this.clicks = clicks;
-  products.push(this); // take the new object that is created, and put it into the array
+  // take the new object that is created, and put it into the array
+  products.push(this);
 }
 
 // if there is nothing in localStorage for the products:
@@ -115,41 +116,5 @@ function handleImgClick(event) {
 img1.addEventListener("click", handleImgClick);
 img2.addEventListener("click", handleImgClick);
 img3.addEventListener("click", handleImgClick);
-
-// function to create a new chart
-function renderChart() {
-  const ctx = document.getElementById("myChart");
-
-  const labels = [];
-  const views = [];
-  const clicks = [];
-
-  // loop through my products array and add in the label, views and clicks data to my arrays
-  for (let i = 0; i < products.length; i++) {
-    labels.push(products[i].name);
-    views.push(products[i].views);
-    clicks.push(products[i].clicks);
-  }
-
-  new Chart(ctx, {
-    type: "bar",
-    data: {
-      labels: labels,
-      datasets: [
-        {
-          label: "# of views",
-          data: views,
-          borderWidth: 1,
-        },
-        {
-          type: "line",
-          label: "# of clicks",
-          data: clicks,
-          borderWidth: 1,
-        },
-      ],
-    },
-  });
-}
 
 renderProducts();
